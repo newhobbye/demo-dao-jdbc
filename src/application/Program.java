b705eafdde51594d39e649cc2439e4b6600ccebd
newhobbye <robsonhobbye@gmail.com>
2021-10-19 05:22:52 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department obj = new Department(1, "livros");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao(); //injeção de dependencia.
 		System.out.println(seller);
 	}
 
