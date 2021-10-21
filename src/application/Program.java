0cf545032e24648d1c4d9b126bd9da8a4513d450
newhobbye <robsonhobbye@gmail.com>
2021-10-21 05:46:27 -0300

    Implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "livros");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao(); //injeção de dependencia.
+		
+		Seller seller = sellerdao.findById(3);
 		System.out.println(seller);
 	}
 
