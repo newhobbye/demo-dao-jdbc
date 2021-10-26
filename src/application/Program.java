631e1b6c3c2e3c02a022d8d3c8947b7496152ffa
newhobbye <robsonhobbye@gmail.com>
2021-10-26 05:31:13 -0300

    implementação do FindByDepartmentId

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -14,6 +17,13 @@ public class Program {
 		System.out.println("=== Teste 1, Seller findById ===");
 		Seller seller = sellerdao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("=== Teste 2, Seller findByDepartmentId ===");
+		Department department = new Department(2, null);
+		List <Seller> list = sellerdao.findByDepartment(department);
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DBException;
@@ -86,6 +89,49 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public List<Seller> findAll() {
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+
+		st = conn.prepareStatement(
+				"SELECT seller.*,department.Name as DepName "
+				+ "FROM seller INNER JOIN department "
+				+ "ON seller.DepartmentId = department.Id "
+				+ "WHERE DepartmentId = ? "
+				+ "ORDER BY Name"
+				);
+		
+		st.setInt(1, department.getId());
+		rs = st.executeQuery();
+		
+		List<Seller> list = new ArrayList<>();
+		Map <Integer, Department> map = new HashMap<>(); //Map: Usado para controlar instanciação do Department repetida
+		
+		while(rs.next()) { //Nesse caso, por ser um resultado multiplo, é necessario o uso do while(enquanto);
+			
+			Department dep = map.get(rs.getInt("DepartmentId")); //consulta no valor da coluna se esse Department Id já existe.
+			if(dep == null) {
+			dep = instanciateDepartment(rs);
+			
+			map.put(rs.getInt("DepartmentId"), dep);
+			
+			}
+			Seller obj = instanciateSeller(rs, dep);
+			list.add(obj);
+			
+		}
+		return list;
+		}catch(SQLException e) {
+			throw new DBException(e.getMessage());
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 }
