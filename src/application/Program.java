c3f9801b73f9bf7ca5c58a97b1007b7d7d8c681d
newhobbye <robsonhobbye@gmail.com>
2021-10-27 05:30:06 -0300

    Implementação do Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -33,9 +32,18 @@ public class Program {
 			
 			System.out.println("=== Teste 4, Seller Insert ===");
 			
-			Seller newSeller = new Seller(null, "Robson", "Robson@g", new Date(), 3000.0, department);
-			sellerdao.insert(newSeller);
-			System.out.println("Inserido! id = " + newSeller.getId());
+			/*
+			 * Seller newSeller = new Seller(null, "Robson", "Robson@g", new Date(), 3000.0,
+			 * department); sellerdao.insert(newSeller);
+			 * System.out.println("Inserido! id = " + newSeller.getId());
+			 */
+			
+			System.out.println("=== Teste 5, Seller Update ===");
+			
+			seller = sellerdao.findById(1);
+			seller.setName("Martha Wayne");
+			sellerdao.update(seller);
+			System.out.println("Update complete!");
 
 		}
 	}
